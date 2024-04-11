1ebc176cfa5d16081bed71d4a93ee6348a73c588
ShomaliParty <persiashahdi@gmail.com>
2024-04-10 20:19:45 -0400

    App done ish

--- top-pick/src/app/app.component.ts
+++ top-pick/src/app/app.component.ts
@@ -1,13 +1,75 @@
-import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
+
+interface Topic {
+  name: string;
+  description: string;
+  interest: number;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    FormsModule,
+    RouterOutlet,
+    CommonModule,
+    MatSliderModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'top-pick';
+  topics: Topic[] = [];
+  newTopicName: string = '';
+  newTopicDescription: string = '';
+
+  constructor() {
+    this.topics.push({
+      name: "testName",
+      description: "test description",
+      interest: 1
+    })
+    this.topics.push({
+      name: "testName2",
+      description: "test description2",
+      interest: 2
+    })
+  }
+
+  addTopic(): void {
+    const newTopic: Topic = {
+      name: this.newTopicName,
+      description: this.newTopicDescription,
+      interest: 1
+    };
+
+    console.log(newTopic);
+
+    this.topics.push(newTopic);
+
+    this.newTopicName = '';
+    this.newTopicDescription = '';
+  }
+
+  sortTopics(): void {
+    this.topics.sort((a, b) => {
+      return b.interest - a.interest;
+    });
+    console.log(this.topics);
+  }
+
 }
